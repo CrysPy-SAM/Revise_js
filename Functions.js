f86912efe8d65ba67cfdb0d1ffb4a29cd2e2143e
Cryspysam <ssmishra9918@gmail.com>
2025-07-01 10:08:39 +0530

    Initial commit

---
+++ Functions.js
@@ -0,0 +1,25 @@
+//Finction Statement
+
+function a(){
+    console.log("a called");
+}
+a();
+
+//Finction Expression
+
+var b = function b(){
+    console.log("b called");
+}
+b();
+
+//Finction Declaration
+
+//Anonymous Finction 
+
+//Need Finction Expression
+
+//Difference between Parameter and Argument ?
+
+//First Class Function
+
+//Arrow Finction 
