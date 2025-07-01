141197c8b58240541fa65510a77464b8b13f2ff3
Cryspysam <ssmishra9918@gmail.com>
2025-07-01 16:23:36 +0530

    Initial commit

--- MapFilterReduce.js
+++ MapFilterReduce.js
@@ -1,16 +1,52 @@
-const arr = [5,1,3,7];
+// const arr = [5,1,3,7];
 
-function double(x) {
-    return x * 2;
+// function double(x) {
+//     return x * 2;
+// }
+
+// function triple(x) {
+//     return x *3;
+// }
+
+
+
+// const output = arr.map((x) => {
+//     x.toString(2)
+// });
+
+// console.log(output);
+
+
+
+// const arr = [1,3,5,3,6,9,0];
+
+// //filter odd values
+
+// function isOdd(x){
+//     return x%2;
+// }
+
+// const output = arr.filter(isOdd);
+// console.log(output);
+
+
+const arr = [1,3,5,3,6,9,0];
+
+//reduce
+//sum or max
+
+function findSum(arr) {
+    let sum = 0;
+    for (let i = 0; i<arr.length; i++) {
+        sum = sum + arr[1];
+    }
+    return sum;
 }
+console.log(findSum(arr));
 
-function triple(x) {
-    return x *3;
-}
+const outpur = arr.reduce(function(acc, curr){
+    acc = acc + curr;
+    return acc;
+}, 0);
 
-
-
-const output = arr.map((x) => {
-    x.toString(2)
-});
-console.log(output);
+console.log(outpur)
