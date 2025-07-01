4c9763ae7ac7c92a448e313d95ca0d2d495b64f3
Cryspysam <ssmishra9918@gmail.com>
2025-07-01 09:54:02 +0530

    Initial commit

--- SetTimeOut.js
+++ SetTimeOut.js
@@ -1,9 +1,23 @@
+// function x() {
+//     var i=1;
+//     for(let i=1; i<=5; i++){
+//      setTimeout(function () {
+//         console.log(i);
+//     },i*1000);
+//     }
+//     console.log("Namste JavaScript");
+// }
+// x();
+
 function x() {
     var i=1;
-    for(let i=1; i<=5; i++){
-     setTimeout(function () {
+    for(var i=1; i<=5; i++){
+       function close(i) {
+            setTimeout(function () {
         console.log(i);
     },i*1000);
+       }
+       close(i);
     }
     console.log("Namste JavaScript");
 }
