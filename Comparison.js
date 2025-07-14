89ad44b7504298df1d170cafb90c9ef83272b9a6
Cryspysam <ssmishra9918@gmail.com>
2025-07-14 14:42:26 +0530

    Initial commit

---
+++ Comparison.js
@@ -0,0 +1,14 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+// everything need to match String === String
+console.log("2" > 2);
+
+
+
