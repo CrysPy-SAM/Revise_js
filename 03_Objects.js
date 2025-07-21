e585bea9d577adcb6a23b6c806909c901f0853be
CrysPy-SAM <ssmishra9918@gmail.com>
2025-07-21 17:51:28 +0530

    obj

---
+++ 03_Objects.js
@@ -0,0 +1,18 @@
+// crete obje
+
+
+//Obje litrals
+
+const JsUser = {
+    name: "Satyam",
+    "full name": "satyam mishra",
+    age: 18,
+    location: "prayagraj",
+    Email: "ssmishra@gmail.com",
+    isLoggedIn: false,
+    lastLoginDate: ["Monday","Tursday"]
+}
+
+console.log(JsUser.name);
+console.log(JsUser["name"]);
+console.log(JsUser["full name"]);
