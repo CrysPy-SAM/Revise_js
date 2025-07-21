e27aacb0d25b31a6b46c003ca323999f09da8e27
CrysPy-SAM <ssmishra9918@gmail.com>
2025-07-22 00:19:53 +0530

    Your commit message here

--- 03_Objects.js
+++ 03_Objects.js
@@ -15,4 +15,36 @@ const JsUser = {
 
 console.log(JsUser.name);
 console.log(JsUser["name"]);
-console.log(JsUser["full name"]);
+console.log(JsUser["full name"]);   // u can not access the without this way
+
+//const tinderuser = new object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Superman"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const newtinderuser = {
+    email: "ssmishra@gmail.com",
+    fullname: {
+         userfullname:{
+               firstname: "Satyam",
+               lastName: "Mishra"
+         }
+    }
+}
+
+// console.log(newtinderuser.fullname);
+
+
+
+const obj1 = { 1: "ABC", 2: "abc" };
+const obj2 = { 3: "DEF", 4: "def" };
+const obj3 = { 5: "GHI", 6: "ghi" };
+
+const obj4 = {...obj1, ...obj2}
+console.log(obj4)
